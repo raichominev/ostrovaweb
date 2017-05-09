ade7af64da7a97c08e90690386439b17629de0ad
ana_lardeva <ana_lardeva@abv.bg>
2017-05-09 07:29:26 +0300

    .

--- ostrovaweb/settings.py
+++ ostrovaweb/settings.py
@@ -260,4 +260,4 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID',None)
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
 AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', None)
 MEDIA_URL = 'http://%s.s3.amazonaws.com/upload/' % str(AWS_STORAGE_BUCKET_NAME)
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
